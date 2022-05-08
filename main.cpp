980f071ed47c18c8ddb1ba304efddf85106b4710
areggie <mixedworldcitizen@gmail.com>
2022-05-08 17:00:56 +0300

    started a new branch following F's method and D's simplified way added main.cpp with ft::vector

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: areggie <areggie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 12:32:02 by areggie           #+#    #+#             */
-/*   Updated: 2022/05/05 12:32:31 by areggie          ###   ########.fr       */
+/*   Updated: 2022/05/08 16:58:08 by areggie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,23 @@ int main ()
   std::vector<int> third (second.begin(),second.end());  // iterating through second
   std::vector<int> fourth (third);                       // a copy of third
 
+  ft::vector<int> first;                                // empty vector of ints
+  ft::vector<int> second (4,100);                       // four ints with value 100
+  ft::vector<int> third (second.begin(),second.end());  // iterating through second
+  ft::vector<int> fourth (third);                       // a copy of third
+
   // the iterator constructor can also be used to construct from arrays:
   int myints[] = {16,2,77,29};
   std::vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
+  ft::vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
 
-  std::cout << "The contents of fifth are:";
+
+  std::cout << "The contents of fifth in std::vector are:";
+  for (std::vector<int>::iterator it = fifth.begin(); it != fifth.end(); ++it)
+    std::cout << ' ' << *it;
+  std::cout << '\n';
+
+  std::cout << "The contents of fifth in ft::vector are:";
   for (std::vector<int>::iterator it = fifth.begin(); it != fifth.end(); ++it)
     std::cout << ' ' << *it;
   std::cout << '\n';
